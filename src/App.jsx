422b269259549ed309cc129345e3902074fae652
mihan2002 <mihanamajith20@gmail.com>
2024-09-04 10:37:31 +0530

    app.jsx set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import UserCreateAccount from "./pages/UserCreateAccount";
+import UserDoctorProfile from "./pages/UserDoctorProfile";
+import UserDoctosList from "./pages/UserDoctosList";
+import UserLogin from "./pages/UserLogin";
+import UserMainDashboard from "./pages/UserMainDashboard";
 
 function App() {
-  return(
-    <div className="App">hello</div>
-  )
-  
+  return (
+    <div className="App">
+      <UserCreateAccount />
+    </div>
+  );
 }
 
 export default App;
